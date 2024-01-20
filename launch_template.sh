f7b6f6357d62578161e846a3662f00de4f528a3f
Brenton Jackson <bjavonjackson@gmail.com>
2024-01-20 18:18:07 -0500

    created launch template

---
+++ launch_template.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install stress-ng -y
+sudo apt-get install htop -y
+sudo apt-get install python3-pip -y
+pip3 install flask
+sudo apt-get install git -y
+cd /home/ubuntu
+git clone git@github.com:brentonjackson/MP2.git
+cd /home/ubuntu/MP2
+python3 serve.py
